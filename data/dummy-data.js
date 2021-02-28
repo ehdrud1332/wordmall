983c34a1a0c374c22e4b1502e844d1e66770eb0f
castle.jaemin <ehdrud1332@gmail.com>
2021-02-28 19:19:35 +0900

    #1 create dummy-data & model

---
+++ data/dummy-data.js
@@ -0,0 +1,12 @@
+import Product from '../models/product';
+
+const PRODUCTS = [
+    new Product(
+        'pl',
+        'ul',
+        'Red Shirt',
+        'https://5.imimg.com/data5/XP/PH/MY-57047128/round-neck-t-shirt-500x500.jpg',
+        29,99
+    ),
+    // ...?
+];
